63d9fb75cba50f74eef7eb23a84e56046af9f122
Gyorgy Szuhanyi <gyorgy.szuhanyi@tba.group>
2023-06-24 15:04:45 +0300

    remove relu from LSTM (for some reason.. )

--- model.jl
+++ model.jl
@@ -1,11 +1,12 @@
-import Flux
 
 module Model
+    import Flux
+
     export model
 
     model = Flux.Chain(
-        Flux.LSTM(3 => 32, relu),
-        Flux.Dense(32 => 3, identity),
+        Flux.LSTM(3 => 32),
+        Flux.Dense(32 => 3, Flux.identity),
         Flux.softmax
     )
 end # Model
