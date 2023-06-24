e4a02f2946c479d1375ea8f43833083cbb9e7512
PortikAbel <portikusz0@gmail.com>
2023-06-24 12:37:29 +0300

    dataset functions for dataloader

--- prepare_data.jl
+++ prepare_data.jl
@@ -1,8 +1,9 @@
-import DataFrames, CSV
 include("./config.jl")
 include("./exception.jl")
-import .DataCfgs
-import .PExcpts
+
+import DataFrames, CSV
+import .DataCfgs, .PExcpts
+import Base.length, Base.getindex
 
 
 struct Recording
@@ -55,20 +56,38 @@ function loadFilesAndEvents()
     end
 
     frequencies = [files_dict[rec_id].frequency for rec_id in events.Id]
-    events.Init = round.(events.Init .* frequencies)
-    events.Completion = round.(events.Completion .* frequencies)
+    events.Init = convert.(Int64, round.(events.Init .* frequencies))
+    events.Completion = convert.(Int64, round.(events.Completion .* frequencies))
 
     events, files_dict
 end
 
 
-struct DataLoader
+struct DataSet
     events::DataFrames.DataFrame
     files_dict::Dict{String,Recording}
+    D::Int64
 end
 
 
-DataLoader() = begin
+DataSet(D::Int64) = begin
     events, files_dict = loadFilesAndEvents()
-    DataLoader(events, files_dict)
+    DataSet(events, files_dict, D)
+end
+
+
+function length(data::DataSet)
+    DataFrames.nrow(data.events)
+end
+
+
+function getindex(data::DataSet, idx::UnitRange{Int64})
+    offset = data.D-1
+    min_idx = data.events.Init[idx]
+    max_idx = data.events.Completion[idx] .- offset
+    start_idx = [rand(s:e) for (s, e) in Iterators.zip(min_idx, max_idx)]
+    cat([
+            Matrix(data.files_dict[rec_id].record[s_idx:s_idx+offset,:])
+            for (s_idx, rec_id) in Iterators.zip(start_idx, data.events.Id[idx])
+    ]..., dims=3)
 end
